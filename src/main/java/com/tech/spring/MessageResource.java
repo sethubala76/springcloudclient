27b64959cb2521035e09d3bf474c0e0b38a1a374
bala <bala@bala.com>
2018-11-25 23:53:44 +0530

    Spring Client Code

---
+++ src/main/java/com/tech/spring/MessageResource.java
@@ -0,0 +1,21 @@
+package com.tech.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RequestMapping("/rest")
+@RestController
+public class MessageResource {
+
+    @Value("${message: Default hello}")
+    private String message;
+
+    @GetMapping("/message")
+    public String message(){
+    return message;
+        }
+}
